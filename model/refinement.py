ccf3ca563996722890d3b6bbfb0c772610a05353
stsan9 <stsan@ucsd.edu>
2022-11-13 09:50:34 +0000

    merge files together

--- model/refinement.py
+++ model/refinement.py
@@ -1,3 +1,4 @@
+
 import torch
 import torch.nn as nn
 import numpy as np
@@ -27,3 +28,32 @@ class GNN(nn.Module):
         # x : [N, 30, 3]
         x = self.gcn(x, self.edge_index)
         return x
+
+class FCN(nn.Module):
+    '''
+    Fully connected refine network
+    '''
+    def __init__(self, p=30, c=3):
+        """
+            args:
+                p: # particles, expected 30
+                c: # features per particle, expected 3
+        """
+        super(FCN, self).__init__()
+        self.p = p
+        self.c = c
+        self.model = nn.Sequential(
+                    nn.Linear(p*c, 512),
+                    nn.ReLU(),
+                    nn.Linear(512, 512),
+                    nn.ReLU(),
+                    nn.Linear(512, 512),
+                    nn.ReLU(),
+                    nn.Linear(512, p*c)
+                )
+
+    def forward(self, x):
+        """
+            x shape: (n, p, c)
+        """
+        return self.model(torch.flatten(x, start_dim=1, end_dim=-1)).reshape(x.shape[0], self.p, self.c)
