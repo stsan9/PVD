2a2b94264805896a43621032624d5ba7e2da0813
stsan9 <stsan@ucsd.edu>
2023-03-10 11:26:33 +0000

    remove float64; use float32 (float)

--- sampler/heun_sampler.py
+++ sampler/heun_sampler.py
@@ -35,11 +35,12 @@ def sample(data, model, opt,
     latents = torch.randn_like(data, device=device)
 
     # t_steps: list of noise levels from sigma_max to sigma_min
-    step_indices = torch.arange(num_steps, dtype=torch.float64, device=device)
+    step_indices = torch.arange(num_steps, dtype=torch.float, device=device)
     t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
     t_steps = torch.cat([torch.as_tensor(t_steps), torch.zeros_like(t_steps[:1])])      # appends 0 at end
+    print(t_steps)
 
-    x_next = latents.to(torch.float64) * t_steps[0]     # scales noise by sigma_max
+    x_next = latents.to(torch.float) * t_steps[0]     # scales noise by sigma_max
     for i, (t_cur, t_next) in list(enumerate(zip(t_steps[:-1], t_steps[1:]))):  # 0, ..., N-1
         x_cur = x_next
 
@@ -51,26 +52,25 @@ def sample(data, model, opt,
 
         # Euler step.
         # denoised = net(x_hat, t_hat).to(torch.float64)        # original code for when model outputs denoised sample rather than noise
-        denoised = x_hat - t_hat * model._denoise(x_hat, t_hat).to(torch.float64)
+        denoised = x_hat - t_hat * model._denoise(x_hat, t_hat).to(torch.float)
         d_cur = (x_hat - denoised) / t_hat
         x_next = x_hat + (t_next - t_hat) * d_cur
 
         # Apply 2nd order correction.
         if i < num_steps - 1:
             # denoised = net(x_next, t_next).to(torch.float64)  # original code
-            denoised = x_next - t_next * model._denoise(x_next, t_next).to(torch.float64)
+            denoised = x_next - t_next * model._denoise(x_next, t_next).to(torch.float)
             d_prime = (x_next - denoised) / t_next
             x_next = x_hat + (t_next - t_hat) * (0.5 * d_cur + 0.5 * d_prime)
-    print(x_next - x_next)
 
     # NOTE: still need to rescale x_next based on mean and std of data (done in caller function)
     return x_next
 
 # TODO: delete after debugging done
-# if __name__ == '__main__':
-#     data = torch.rand(100,3)
-#     opt = lambda: None
-#     opt.nc = 3
-#     model = lambda: None
-#     model._denoise = lambda x,t: x
-#     sample(data, model, opt)
+if __name__ == '__main__':
+    data = torch.rand(100,3)
+    opt = lambda: None
+    opt.nc = 3
+    model = lambda: None
+    model._denoise = lambda x,t: x
+    sample(data, model, opt)
--- test_generation.py
+++ test_generation.py
@@ -278,7 +278,7 @@ class Model(nn.Module):
 
     def _denoise(self, data, t):
         B, D, N= data.shape
-        assert data.dtype == torch.float or data.dtype == torch.float64
+        assert data.dtype == torch.float
         if t.dim() == 0:
             t = t.repeat(B) # convert scalar to vector
         assert t.shape == torch.Size([B]) # and t.dtype == torch.int64 NOTE: we can use floats as per EDM
