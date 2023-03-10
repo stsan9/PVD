68cdf097b29565c78c58d394b3941f806018cd8e
stsan9 <stsan@ucsd.edu>
2023-03-10 00:13:28 +0000

    rm pvd unused code for merging

--- test_generation.py
+++ test_generation.py
@@ -26,35 +26,6 @@ from datasets.jetnet import load_gluon_dataset
 '''
 models
 '''
-def normal_kl(mean1, logvar1, mean2, logvar2):
-    """
-    KL divergence between normal distributions parameterized by mean and log-variance.
-    """
-    return 0.5 * (-1.0 + logvar2 - logvar1 + torch.exp(logvar1 - logvar2)
-                + (mean1 - mean2)**2 * torch.exp(-logvar2))
-
-def discretized_gaussian_log_likelihood(x, *, means, log_scales):
-    # Assumes data is integers [0, 1]
-    assert x.shape == means.shape == log_scales.shape
-    px0 = Normal(torch.zeros_like(means), torch.ones_like(log_scales))
-
-    centered_x = x - means
-    inv_stdv = torch.exp(-log_scales)
-    plus_in = inv_stdv * (centered_x + 0.5)
-    cdf_plus = px0.cdf(plus_in)
-    min_in = inv_stdv * (centered_x - .5)
-    cdf_min = px0.cdf(min_in)
-    log_cdf_plus = torch.log(torch.max(cdf_plus, torch.ones_like(cdf_plus)*1e-12))
-    log_one_minus_cdf_min = torch.log(torch.max(1. - cdf_min,  torch.ones_like(cdf_min)*1e-12))
-    cdf_delta = cdf_plus - cdf_min
-
-    log_probs = torch.where(
-    x < 0.001, log_cdf_plus,
-    torch.where(x > 0.999, log_one_minus_cdf_min,
-             torch.log(torch.max(cdf_delta, torch.ones_like(cdf_delta)*1e-12))))
-    assert log_probs.shape == x.shape
-    return log_probs
-
 
 class GaussianDiffusion:
     def __init__(self,betas, loss_type, model_mean_type, model_var_type):
@@ -368,88 +339,14 @@ def get_betas(schedule_type, b_start, b_end, time_num):
         raise NotImplementedError(schedule_type)
     return betas
 
-def get_constrain_function(ground_truth, mask, eps, num_steps=1):
-    '''
-
-    :param target_shape_constraint: target voxels
-    :return: constrained x
-    '''
-    # eps_all = list(reversed(np.linspace(0,np.float_power(eps, 1/2), 500)**2))
-    eps_all = list(reversed(np.linspace(0, np.sqrt(eps), 1000)**2 ))
-    def constrain_fn(x, t):
-        eps_ =  eps_all[t] if (t<1000) else 0
-        for _ in range(num_steps):
-            x  = x - eps_ * ((x - ground_truth) * mask)
-
-
-        return x
-    return constrain_fn
-
-
-#############################################################################
-
-def get_dataset(dataroot, npoints,category,use_mask=False):
-    tr_dataset = ShapeNet15kPointClouds(root_dir=dataroot,
-        categories=[category], split='train',
-        tr_sample_size=npoints,
-        te_sample_size=npoints,
-        scale=1.,
-        normalize_per_shape=False,
-        normalize_std_per_axis=False,
-        random_subsample=True, use_mask = use_mask)
-    te_dataset = ShapeNet15kPointClouds(root_dir=dataroot,
-        categories=[category], split='val',
-        tr_sample_size=npoints,
-        te_sample_size=npoints,
-        scale=1.,
-        normalize_per_shape=False,
-        normalize_std_per_axis=False,
-        all_points_mean=tr_dataset.all_points_mean,
-        all_points_std=tr_dataset.all_points_std,
-        use_mask=use_mask
-    )
-    return tr_dataset, te_dataset
-
-
-
-# def evaluate_gen(opt, ref_pcs, logger):
-
-#     if ref_pcs is None:
-#         _, test_dataset = get_dataset(opt.dataroot, opt.npoints, opt.category, use_mask=False)
-#         test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.batch_size,
-#                                                       shuffle=False, num_workers=int(opt.workers), drop_last=False)
-#         ref = []
-#         for data in tqdm(test_dataloader, total=len(test_dataloader), desc='Generating Samples'):
-#             x = data['test_points']
-#             m, s = data['mean'].float(), data['std'].float()
-
-#             ref.append(x*s + m)
-
-#         ref_pcs = torch.cat(ref, dim=0).contiguous()
-
-#     logger.info("Loading sample path: %s"
-#       % (opt.eval_path))
-#     sample_pcs = torch.load(opt.eval_path).contiguous()
-
-#     logger.info("Generation sample size:%s reference size: %s"
-#           % (sample_pcs.size(), ref_pcs.size()))
-
-
-#     # Compute metrics
-#     results = compute_all_metrics(sample_pcs, ref_pcs, opt.batch_size)
-#     results = {k: (v.cpu().detach().item()
-#                    if not isinstance(v, float) else v) for k, v in results.items()}
-
-#     pprint(results)
-#     logger.info(results)
-
-#     jsd = JSD(sample_pcs.numpy(), ref_pcs.numpy())
-#     pprint('JSD: {}'.format(jsd))
-#     logger.info('JSD: {}'.format(jsd))
-
-
 
 def generate(model, opt):
+    """
+    Main sampling loop
+
+    model: diffusion model
+    opt: terminal args object
+    """
 
     if opt.category == 'gluon':
         test_dataset = load_gluon_dataset(opt.dataroot, opt.dataset_size, generate=True)
@@ -475,17 +372,11 @@ def generate(model, opt):
             gen = gen.transpose(1,2).contiguous()
             x = x.transpose(1,2).contiguous()
 
-
-
-            import pdb; pdb.set_trace()
             gen = gen * s + m
             x = x * s + m
             
             samples.append(gen)
             ref.append(x)
-
-            # visualize_pointcloud_batch(os.path.join(str(Path(opt.eval_path).parent), 'x.png'), gen[:64], None,
-            #                            None, None)
 
         samples = torch.cat(samples, dim=0)
         ref = torch.cat(ref, dim=0)
@@ -493,18 +384,12 @@ def generate(model, opt):
         torch.save(samples, opt.eval_path)
         torch.save(ref, opt.eval_path[:-len('samples.pth')] + 'ref.pth')
 
-
-
     return ref
 
 
 def main(opt):
 
-    if opt.category == 'airplane':
-        opt.beta_start = 1e-5
-        opt.beta_end = 0.008
-        opt.schedule_type = 'warm0.1'
-
+    # set up directory paths
     exp_id = os.path.splitext(os.path.basename(__file__))[0]
     dir_id = os.path.dirname(__file__)
     output_dir = get_output_dir(dir_id, exp_id)
@@ -513,6 +398,7 @@ def main(opt):
 
     outf_syn, = setup_output_subdirs(output_dir, 'syn')
 
+    # get noise schedule and diffusion model settings
     betas = get_betas(opt.schedule_type, opt.beta_start, opt.beta_end, opt.time_num)
     model = Model(opt, betas, opt.loss_type, opt.model_mean_type, opt.model_var_type)
 
@@ -522,11 +408,11 @@ def main(opt):
     def _transform_(m):
         return nn.parallel.DataParallel(m)
 
+    # put model on gpu and parallelize if necessary
     model = model.cuda()
     model.multi_gpu_wrapper(_transform_)
 
     model.eval()
-
     with torch.no_grad():
 
         logger.info("Resume Path:%s" % opt.model)
@@ -534,16 +420,11 @@ def main(opt):
         resumed_param = torch.load(opt.model)
         model.load_state_dict(resumed_param['model_state'])
 
-
         ref = None
         if opt.generate:
             opt.eval_path = os.path.join(outf_syn, 'samples.pth')
             Path(opt.eval_path).parent.mkdir(parents=True, exist_ok=True)
             ref=generate(model, opt)
-            
-        # if opt.eval_gen:
-        #     # Evaluate generation
-        #     evaluate_gen(opt, ref, logger)
 
 
 def parse_args():
@@ -581,7 +462,6 @@ def parse_args():
     parser.add_argument('--model', default='',required=True, help="path to model (to continue training)")
 
     '''eval'''
-
     parser.add_argument('--eval_path',
                         default='')
 
